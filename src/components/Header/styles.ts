51bb842744c3f83acad0008828282ab296b2f2a9
Lindsay Ferreira <lindsayferreira@ioasys.com.br>
2021-10-27 11:31:36 -0300

    feat: create header media query

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,4 +5,8 @@ export const Container = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 42px 120px ;
+
+  @media (max-width: 768px) {
+    padding:  40px 20px;
+}
 `;
